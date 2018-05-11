ffa1e5821d56b36435cd6c7f4a31382e523b9816
neonrights <nicholasfarn@gmail.com>
2018-05-11 11:59:22 -0700

    cleaning code for constraint propagation

--- solver.py
+++ solver.py
@@ -65,66 +65,62 @@ class CSPSolver(MinesweeperSolver):
                     except IndexError:
                         continue
 
-        if not constraint_vars:
-            assert constraint_val == 0
-            return # no variables in constraint, no need to add
-
         # remove known mines from constraint, update constraint
         constraint_val -= len(constraint_vars.intersection(self.known_mines))
         constraint_vars = constraint_vars.difference(self.known_mines)
         assert constraint_val >= 0
 
-        resolved_constraints = collections.deque()
-        if constraint_val == 0:
-            resolved_constraints.append([constraint_vars, constraint_val])
-            self.save_moves = self.safe_moves.union(constraint_vars)
-        elif len(constraint_vars) == constraint_val:
-            # prune known mines
-            resolved_constraints.append([constraint_vars, constraint_val])
-            self.known_mines = self.known_mines.union(constraint_vars)
-
-        # constraint not resolved, prune sets where new constraint contains subset of variables
-        if not resolved_constraints:
-            for i in range(len(self.constraints)):
-                if self.constraints[i][0].issuperset(constraint_vars):
-                    self.constraints[i][0] = self.constraints[i][0].difference(constraint_vars)
-                    self.constraints[i][1] -= constraint_val
-                    # check if constraint has been resolved
-                elif self.constraints[i][0].issubset(constraint_vars):
-                    break # edit constraint variables, see if resolved
-                    # if resolved, add to resolved list and break?
-                    # if not continue? restart? with new constraints
-
-            if not resolved_constraints:
-                return # no more constraints to prune
+        new_constraints = collections.deque()
+        new_constraints.append(set([position]), 0) # prune newly revealed space
+        if not constraint_vars:
+            assert constraint_val == 0
+            new_constraints.append([constraint_vars, constraint_val]) # prune 
+            if constraint_val == 0:
+                self.save_moves = self.safe_moves.union(constraint_vars)
+            elif len(constraint_vars) == constraint_val:
+                self.known_mines = self.known_mines.union(constraint_vars)
         
         # continue while there are still variables to prune
-        while not resolved_constraints:
-            constraint_vars, constraint_val = resolved_constraints.popleft()
+        while not new_constraints:
+            constraint_vars, constraint_val = new_constraints.popleft()
             for i in range(len(self.constraints)):
                 if constraint_val == 0:
+                    # resolved constraint, all safe
                     self.constraints[i][0] = self.constraints[i][0].difference(constraint_vars)
-                else:
+                elif len(constraint_vars) == constraint_val:
+                    # resolved constraint, all mines
                     self.constraints[i][1] -= len(self.constraints[i][0].intersection(constraint_vars))
                     self.constraints[i][0] = self.constraints[i][0].difference(constraint_vars)
+                else:
+                    # unresolved constraint
+                    if self.constraints[i][0].issuperset(constraint_vars):
+                        # new constraint is subset of old constraint
+                        self.constraints[i][0] = self.constraints[i][0].difference(constraint_vars)
+                        self.constraints[i][1] -= constraint_val
+                    elif self.constraints[i][0].issubset(constraint_vars):
+                        # old constraint is subset of new constraint
+                        new_constraints.append
+                        # edit constraint variables, see if resolved
+                        # if resolved, add to resolved list and break?
+                        # if not continue? restart? with new constraints
 
                 if not self.constraints[i][0]:
                     assert self.constraints[i][1] == 0
-                    del self.constraints[i] # empty constraint
+                    del self.constraints[i] # empty constraint, remove
                     continue
 
-                # if constraint is satisfied, add new variables to list
+                # if constraint is resolved, add new variables to list
                 if self.constraints[i][1] == 0:
-                    resolved_constraints.append(self.constraints[i])
+                    new_constraints.append(self.constraints[i])
                     del self.constraints[i]
                 elif self.constraints[i][0] == self.constraints[i][1]:
-                    resolved_constraints.append(self.constraints[i])
+                    new_constraints.append(self.constraints[i])
                     self.known_mines = self.known_mines.union(self.constraints[i][0])
                     del self.constraints[i]
 
-
-
-
+            if constraint_val == 0 or len(constraint_vars) == constraint_val:
+                # add constraint if not resolved, not altered, and not repeat
+                self.constraints.append([constraint_vars, constraint_val])
 
     def _calculate_probabilities(self):
         raise NotImplementedError
