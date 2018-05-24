03264262c6fd4cecc5c222f0037e6748e3baa1c9
Nicholas Farn <nicholasfarn@gmail.com>
2018-05-23 17:39:15 -0700

    started implementation of CCCSP solver

--- solver.py
+++ solver.py
@@ -20,7 +20,7 @@ class MinesweeperSolver(object):
 
 	def act(self):
 		# determine which space to click on
-		# determine if game s over
+		# determine if game is over
 		raise NotImplementedError
 
 	def save_state(self):
@@ -303,6 +303,7 @@ class CCCSPSolver(CSPSolver):
 		remaining_variables = set([(pos1, pos2) for pos1, pos2 in zip(*np.where(self.board.covered))])
 		remaining_variables -= self.known_mines
 		remaining_variables -= self.safe_moves
+
 		# split unknown variables into disjoint sets of variables and constraints
 		disjoint_sets = list()
 		while remaining_variables:
@@ -330,12 +331,37 @@ class CCCSPSolver(CSPSolver):
 				for var in constraints[0][0]:
 					probabilities[var] = prob
 			else:
-				# TODO split into maximal sets, then run dfs on them
+				# split variables into maximal sets
+				max_var_sets = set()
+				remaining_variables = set(variables)
+				while remaining_variables:
+					var = remaining_variables.pop()
+					max_set = set(variables)
 
-				# do initial ordering of variables by number of constraints they appear in
-				sums, total = self._constraint_dfs(constraints, dict(), 0, list())
-				for var in variables:
-					probabilities[var] = float(sums[var]) / total
+					for constraint in constraints:
+						if var in constraint[0]:
+							max_set = max_set.intersection(constraint[0])
+						else:
+							max_set -= constraint[0]
+
+					remaining_variables -= max_set
+					max_var_sets.add(frozenset(max_set))
+
+				# rewrite constraints in terms of new maximal set variables
+				max_constraints = copy.deepcopy(constraints)
+				for constraint in max_constraints:
+					for max_var in max_var_sets:
+						if constraint[0].issuperset(max_var):
+							constraint[0] -= max_var
+							constraint[0].add(max_var)
+
+				pdb.set_trace()
+				# use dfs to calculate probabilities
+				sums, total = self._constraint_dfs(max_constraints, dict(), 0, list())
+				for max_var, val in sums:
+					set_size = len(max_var)
+					for var in max_var:
+						probabilities[var] = float(val) / (set_size * total)
 
 		# find 0's and 1's
 		for pos, val in probabilities.items():
@@ -353,7 +379,6 @@ class CCCSPSolver(CSPSolver):
 
 
 	def _constraint_dfs(self, constraint_list, sums, total, var_val_pairs):
-		# TODO, run maximal sets
 		if not constraint_list: # all constraints resolved
 			# record variable value pairs in probabilities
 			total += 1
@@ -367,7 +392,7 @@ class CCCSPSolver(CSPSolver):
 		# at each recursion, go through constraint list, select which variable to choose next
 		constraint_counts = dict()
 		for i, constraint in enumerate(constraint_list):
-			assert len(constraint[0]) >= constraint[1]
+			assert sum(len(max_set) for max_set in constraint[0]) >= constraint[1]
 			if constraint[1] == 0:
 				# all must be 0, set all as 0
 				new_constraint_list = copy.deepcopy(constraint_list)
@@ -393,7 +418,7 @@ class CCCSPSolver(CSPSolver):
 				new_var_val_pairs = list(var_val_pairs) + [(var, 0) for var in constraint[0]]
 				sums, total = self._constraint_dfs(new_constraint_list, sums, total, new_var_val_pairs)
 				return sums, total
-			elif len(constraint[0]) == constraint[1]:
+			elif sum([len(max_set) for max_set in constraint[0]]) == constraint[1]:
 				# all must be 1, set all as 1
 				new_constraint_list = copy.deepcopy(constraint_list)
 				del new_constraint_list[i]
@@ -401,8 +426,11 @@ class CCCSPSolver(CSPSolver):
 				# update constraints, look for conflicts
 				delete_set = set()
 				for j, new_constraint in enumerate(new_constraint_list):
-					new_constraint[1] -= len(constraint[0].intersection(new_constraint[0]))
-					new_constraint[0] -= constraint[0]
+					# trickier update
+					common = constraint[0].intersection(new_constraint[0])
+					new_constraint[0] -= common
+					for max_set in common:
+						new_constraint[1] -= len(max_set)
 
 					if new_constraint[1] < 0: # invalid assignment
 						return sums, total
@@ -427,7 +455,7 @@ class CCCSPSolver(CSPSolver):
 					constraint_counts[var] = 1
 
 		chosen_var = max(constraint_counts.items(), key=operator.itemgetter(1))[0]
-		for chosen_val in [0, 1]:
+		for chosen_val in range(len(chosen_var)):
 			# copy of constraints, update constraints based off of chosen value
 			new_constraint_list = copy.deepcopy(constraint_list)
 			delete_set = set()
@@ -531,5 +559,62 @@ def test_csp_dfs():
 	print("hard probability test passed!")
 
 
+@exception_debugger
+def test_cccsp_dfs():
+	# test dfs
+	test_solver = CCCSPSolver((5,5),5)
+	# test board state
+	test_solver.board.covered = np.ones((5,5), dtype=bool)
+	test_solver.board.adjacent_mines = np.array([[ 1,  2,  1, 1, 0],
+												 [-1,  3, -1, 2, 0],
+												 [ 1,  3, -1, 2, 0],
+												 [ 1,  3,  3, 2, 0],
+												 [ 1, -1, -1, 1, 0]])
+	# test constraint list
+	test_solver.board.reveal((2,1))
+	test_solver._add_constraint((2,1))
+
+	assert len(test_solver.constraints) == 2
+
+	# call probabilistic guess, check probabilities, check if disjoint
+	_, probs = test_solver._probabilistic_guess()
+	true_probs = np.array([[1./8, 1./8, 1./8, 1./8, 1./8],
+						   [3./8, 3./8, 3./8, 1./8, 1./8],
+						   [3./8, 0.,   3./8, 1./8, 1./8],
+						   [3./8, 3./8, 3./8, 1./8, 1./8],
+						   [1./8, 1./8, 1./8, 1./8, 1./8]])
+
+	test_probs = np.zeros((5,5))
+	for key in probs:
+		test_probs[key] = probs[key]
+
+	assert np.allclose(test_probs, true_probs)
+	print("basic probability test passed!")
+	
+	# do a harder one
+	test_solver.board.reveal((2,3))
+	test_solver._add_constraint((2,3))
+
+	assert len(test_solver.constraints) == 3
+
+	_, probs = test_solver._probabilistic_guess()
+	prob1 = 57./455
+	prob2 = 159./455
+	prob3 = 38./91
+	prob4 = 68./455
+	true_probs = np.array([[prob1, prob1, prob1, prob1, prob1],
+						   [prob2, prob2, prob3, prob4, prob4],
+						   [prob2, 0.,    prob3, 0.,    prob4],
+						   [prob2, prob2, prob3, prob4, prob4],
+						   [prob1, prob1, prob1, prob1, prob1]])
+
+	test_probs = np.zeros((5,5))
+	for key in probs:
+		test_probs[key] = probs[key]
+
+	assert np.allclose(test_probs, true_probs)
+	print("hard probability test passed!")
+
+
 if __name__ == '__main__':
-	test_csp_dfs()
+	test_cccsp_dfs()
